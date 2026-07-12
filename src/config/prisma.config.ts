aa353389bb0ae7b3b5b720bd297d769a93c6fa88
Makinthan2001 <mahinthan2001a@gmail.com>
2026-07-12 18:54:25 +0530

    initialize Prisma ORM configuration and database schema with PostgreSQL support

--- src/config/prisma.config.ts
+++ src/config/prisma.config.ts
@@ -1,18 +1,16 @@
 import "dotenv/config";
-import { neonConfig } from "@neondatabase/serverless";
-import { PrismaNeon } from "@prisma/adapter-neon";
 import { PrismaClient } from "@prisma/client";
-import ws from "ws";
-
-// Set the WebSocket constructor for the serverless driver to work on Node.js
-neonConfig.webSocketConstructor = ws;
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 if (!globalForPrisma.prisma) {
-  const adapter = new PrismaNeon({ connectionString });
+  const pool = new Pool({ connectionString });
+  const adapter = new PrismaPg(pool);
+  
   globalForPrisma.prisma = new PrismaClient({
     adapter,
     log: ["error", "warn"],
